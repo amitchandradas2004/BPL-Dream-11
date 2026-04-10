a0f2b3efaf7d009f8623291703ad7437add16bb1
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-10 17:41:14 +0600

    All Available Players are loaded in the UI

--- src/Components/Players/Player.jsx
+++ src/Components/Players/Player.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { FaFlag, FaUser } from "react-icons/fa";
 
 const Player = ({ player }) => {
-  console.log(player);
+  // console.log(player);
 
   return (
-    <div>
-      <h2>Name: {player.name}</h2>
-      <h3>Country: {player.country}</h3>
+    <div className="card bg-gray-100   shadow-lg items-center content-center mx-auto py-3 rounded-2xl">
+      <figure>
+        <img
+          src={player.image}
+          alt={player.name}
+          className="w-[90%] h-50 rounded-2xl"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          <FaUser /> {player.name}
+        </h2>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <FaFlag />
+            <p className="font-bold">{player.country}</p>
+          </div>
+          <button className="btn btn-ghost">{player.type}</button>
+        </div>
+        <div className="divider"></div>
+        <h3 className="font-bold">Rating: ({player.rating})</h3>
+        <div className="flex justify-between items-center">
+          <h4>{player.batting}</h4>
+          <h4>{player.bowlling}</h4>
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <h4 className="font-bold">Price: ${player.price}</h4>
+          <button className="btn btn-ghost">Choose Player</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -3,14 +3,15 @@ import Player from "./Player";
 
 const Players = ({ playersPromise }) => {
   const playersData = use(playersPromise);
-  console.log(playersData);
+  // console.log(playersData);
 
   return (
     <div>
-      <h3>Players List : {playersData.length}</h3>
-      {playersData.map((player) => (
-        <Player player={player}></Player>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 container mx-auto">
+        {playersData.map((player) => (
+          <Player key={player.id} player={player}></Player>
+        ))}
+      </div>
     </div>
   );
 };
